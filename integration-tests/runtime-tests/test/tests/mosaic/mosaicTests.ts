285b885c6037388d542270e2ca0e2c0568405464
Dominik Roth <dominik@composable.finance>
2022-03-16 15:56:55 +0100

    test(Runtime Integration Tests): Mosaic Tests W.I.P.

--- integration-tests/runtime-tests/test/tests/mosaic/mosaicTests.ts
+++ integration-tests/runtime-tests/test/tests/mosaic/mosaicTests.ts
@@ -37,7 +37,7 @@ describe('tx.mosaic Tests', function () {
     before(function() {
       sudoKey = walletAlice;
       startRelayerWallet = walletEve.derive("/test/mosaic/relayerWallet");
-      newRelayerWallet = walletEve.derive("test/mosaic/newRelayerWallet");
+      newRelayerWallet = walletEve.derive("/test/mosaic/newRelayerWallet");
     });
 
     /**
@@ -59,12 +59,51 @@ describe('tx.mosaic Tests', function () {
      * Sudo call therefore result is checked by `.isOk`.
      */
     it('Should be able to rotate relayer', async function () {
+      this.skip(); // ToDo: Remove when rotateRelayer() works!
       // Check if this test is enabled.
       if (!testConfiguration.enabledTests.query.account__success.balanceGTZero1)
         this.skip();
       // Setting timeout to 2 minutes.
       this.timeout(2 * 60 * 1000);
       const {data:[result]} = await TxMosaicTests.testRotateRelayer(sudoKey, newRelayerWallet.address, 16);
+      expect(result.isOk).to.be.true;
+    });
+
+    /**
+     * Setting the network.
+     */
+    it('Should be able to rotate relayer', async function () {
+      // Check if this test is enabled.
+      if (!testConfiguration.enabledTests.query.account__success.balanceGTZero1)
+        this.skip();
+      // Setting timeout to 2 minutes.
+      this.timeout(2 * 60 * 1000);
+      const networkId = api.createType('u32', 2)
+      const networkInfo = api.createType('PalletMosaicNetworkInfo', {
+        enabled: api.createType('bool', false),
+        maxTransferSize: api.createType('u128', 100000000000)
+      });
+      const {data:[result]} = await TxMosaicTests.testSetNetwork(sudoKey, networkId, networkInfo);
+      expect(result.isOk).to.be.true;
+    });
+
+    /**
+     * Setting the budget.
+     */
+    it('Should be able set the budget', async function () {
+      // Check if this test is enabled.
+      if (!testConfiguration.enabledTests.query.account__success.balanceGTZero1)
+        this.skip();
+      // Setting timeout to 2 minutes.
+      this.timeout(2 * 60 * 1000);
+      const assetId = api.createType('u128', 2);
+      const amount = api.createType('u128', 1);
+      const decay = api.createType('PalletMosaicDecayBudgetPenaltyDecayer', {
+        Linear:
+          api.createType('PalletMosaicDecayLinearDecayer', {Linear: api.createType('u128', 1), negative: false})
+
+      });
+      const {data:[result]} = await TxMosaicTests.testSetBudget(sudoKey, assetId, amount, decay);
       expect(result.isOk).to.be.true;
     });
   });
@@ -91,6 +130,24 @@ export class TxMosaicTests {
       sudoKey,
       api.events.sudo.Sudid.is,
       api.tx.sudo.sudo(api.tx.mosaic.rotateRelayer(newRelayerWallet, validatedTtl))
+    );
+  }
+
+  public static async testSetNetwork(sudoKey:KeyringPair, networkId, networkInfo) {
+    return await sendAndWaitForSuccess(
+      api,
+      sudoKey,
+      api.events.sudo.Sudid.is,
+      api.tx.sudo.sudo(api.tx.mosaic.setNetwork(networkId, networkInfo))
+    );
+  }
+
+  public static async testSetBudget(sudoKey:KeyringPair, assetId, amount, decay) {
+    return await sendAndWaitForSuccess(
+      api,
+      sudoKey,
+      api.events.sudo.Sudid.is,
+      api.tx.sudo.sudo(api.tx.mosaic.setBudget(assetId, amount, decay))
     );
   }
 }
